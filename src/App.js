dc8279f1877af3fb1734013de7310c607c694498
felipey2010 <mahamaphilip@yahoo.com>
2021-07-03 15:54:42 -0400

    added meter

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import topLeft from "./images/top-left.png";
 import topRight from "./images/logo-cbrr.png";
 import topMiddle from "./images/logo.png";
 import bottomLeft from "./images/bottom-left.png";
+import bottomRight from "./images/right-bottom.png";
+import thermometer from "./images/thermometer.png";
 
 export default function App() {
   return (
@@ -20,6 +22,12 @@ export default function App() {
       </div>
       <div className="bottom-left">
         <img src={bottomLeft} alt="bottom element" />
+      </div>
+      <div className="bottom-right">
+        <img src={bottomRight} alt="bottom element" />
+      </div>
+      <div className="middle">
+        <img src={thermometer} alt="thermometer" />
       </div>
     </div>
   );
