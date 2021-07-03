52cbb4b70a8e2053aa6a2c61e0ffdede23e65936
felipey2010 <mahamaphilip@yahoo.com>
2021-07-03 13:57:00 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./css/App.css";
+import topLeft from "./images/top-left.png";
+import topRight from "./images/logo-cbrr.png";
+import topMiddle from "./images/logo.png";
+import bottomLeft from "./images/bottom-left.png";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="top">
+        <div className="top-left">
+          <img src={topLeft} alt="top image" />
+        </div>
+        <div className="top-middle">
+          <img src={topMiddle} alt="logo" />
+        </div>
+        <div className="top-right">
+          <img src={topRight} alt="logo cbrr" />
+        </div>
+      </div>
+      <div className="bottom-left">
+        <img src={bottomLeft} alt="bottom image" />
+      </div>
     </div>
   );
 }
-
-export default App;
